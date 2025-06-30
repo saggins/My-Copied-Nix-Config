f23a194a7855fb9ec75c4c5d58efc1269d7d0957
Sangmin Chun <sangmin@sagg.in>
2025-06-29 17:57:16 -0700

    added ssh keys

--- home-manager/_mixins/users/sangmin/default.nix
+++ home-manager/_mixins/users/sangmin/default.nix
@@ -1,13 +1,27 @@
-{pkgs, ...}: {
+ 
+{
+  config,
+  hostname,
+  isLima,
+  isWorkstation,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+let
+  inherit (pkgs.stdenv) isLinux;
+in 
+ {
   home = {
-    packages = [
+    packages = lib.mkIf (isLinux && !isLima)  [
       pkgs.zotero
       pkgs.libreoffice
       pkgs.discord
     ];
   };
   wayland.windowManager.hyprland = {
-    extraConfig =''
+    extraConfig = lib.mkIf (isLinux && !isLima)  ''
       exec-once = [workspace 8 silent] /home/sangmin/share/gs.sh
     '';
   };
@@ -17,6 +31,14 @@
       userEmail = "sangmin@sagg.in";
       userName = "Sangmin Chun";
     };
+
+    systemd.user.tmpfiles = lib.mkIf (isLinux && !isLima) {
+    rules = [
+      "d ${config.home.homeDirectory}/Crypt 0755 ${username} users - -"
+      "d ${config.home.homeDirectory}/Vaults/HomeworkFolder 0755 ${username} users - -"
+      "d ${config.home.homeDirectory}/Vaults/Secrets 0755 ${username} users - -"
+    ];
+  };
 
 }
 
