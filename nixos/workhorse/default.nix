1b6842f943cb29417c9951d211759d9eed8a4672
Sangmin Chun <sangmin@sagg.in>
2025-07-07 17:03:19 -0700

    added proper hardware inputs

--- nixos/workhorse/default.nix
+++ nixos/workhorse/default.nix
@@ -8,10 +8,7 @@
 }:
 {
   imports = [
-    inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-gpu-amd
-    inputs.nixos-hardware.nixosModules.common-pc
-    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-hardware.nixosModules.dell-precision-7520
     ./disks.nix
   ];
 
