79031e4bd7c773f810271aa20b4024bed4aef010
Sangmin Chun <sangmin@sagg.in>
2025-08-12 22:42:42 -0700

    trying again

--- nixos/_mixins/services/garage/default.nix
+++ nixos/_mixins/services/garage/default.nix
@@ -9,35 +9,18 @@ let
   installOn = [ "workhorse" ];
 in
 lib.mkIf (lib.elem "${hostname}" installOn) {
+  sops.secrets= {
+    garage-config= {
+      group ="garage";
+      user = "garage";
+      mode = "0644";
+      path = "/mnt/data/garage/garage-config.ini";
+      sopsFile = ../../../../secrets/garage.yaml;
+    };
+  };
   services.garage = {
     enable= true;
     package= pkgs.garage_2;
-    settings = ''
-      db_engine = "sqlite"
-
-      replication_factor = 1
-
-      rpc_bind_addr = "[::]:3901"
-      rpc_public_addr = "127.0.0.1:3901"
-      rpc_secret = "${(builtins.fromTOML config.sops.secrets.garage ).rpc_secret})"
-
-      [s3_api]
-      s3_region = "garage"
-      api_bind_addr = "[::]:3900"
-      root_domain = ".s3.garage.localhost"
-
-      [s3_web]
-      bind_addr = "[::]:3902"
-      root_domain = ".web.garage.localhost"
-      index = "index.html"
-
-      [k2v_api]
-      api_bind_addr = "[::]:3904"
-
-      [admin]
-      api_bind_addr = "[::]:3903"
-      admin_token = "${(builtins.fromTOML config.sops.secrets.garage).admin_token})"
-      metrics_token = "${(builtins.fromTOML config.sops.secrets.garage).metrics_token})"
-    '';
+    settings = builtins.readFile config.sops.secrets.garage-config.path;
   };
 }
