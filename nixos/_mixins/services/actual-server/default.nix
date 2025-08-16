a46d012dd481d78b2b2ab060275ef07609918bff
Sangmin Chun <sangmin@sagg.in>
2025-08-16 15:38:50 -0700

    added tailscale caddy reverse proxy to actual

--- nixos/_mixins/services/actual-server/default.nix
+++ nixos/_mixins/services/actual-server/default.nix
@@ -9,7 +9,16 @@ in
 lib.mkIf (lib.elem hostname installOn) {
   services.actual = {
     enable = true;
-    settings.hostname = "10.0.0.179";
   };
+
+  services.caddy = lib.mkIf (config.services.actual.enable) {
+    enable = true;
+    virtualHosts."actual.${hostname}.ts.net" = {
+      extraConfig = ''
+        reverse_proxy ${config.services.actual.hostname}:${config.services.actual.port}
+      '';
+    }
+
+  }
   # networking.firewall.trustedInterfaces = [ "enp0s31f6" ];
 }
