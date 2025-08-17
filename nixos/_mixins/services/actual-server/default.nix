0d5e2155398ca04e86b5b69a672251f11fc863bf
Sangmin Chun <sangmin@sagg.in>
2025-08-16 17:24:27 -0700

    fixed pathing

--- nixos/_mixins/services/actual-server/default.nix
+++ nixos/_mixins/services/actual-server/default.nix
@@ -13,17 +13,15 @@ lib.mkIf (lib.elem hostname installOn) {
   services.actual = {
     enable = true;
     settings.hostname = "127.0.0.1";
+    settings.webRoot = basePath;
   };
 
   services.caddy = lib.mkIf (config.services.actual.enable && config.services.tailscale.enable) {
     enable = true;
     virtualHosts."${hostname}.${tailNet}" = {
       extraConfig = ''
-        redir ${basePath} ${basePath}/
         handle_path ${basePath}/* {
           reverse_proxy ${config.services.actual.settings.hostname}:${builtins.toString config.services.actual.settings.port} {
-            header_up ${config.services.actual.settings.hostname}
-          }
         }
       '';
     };
