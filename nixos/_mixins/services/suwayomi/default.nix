54d8cd48f025aa92bfcce960a93ea535a16fca02
Sangmin Chun <sangmin@sagg.in>
2025-08-21 17:03:43 -0700

    refactor and fixing syntax

--- nixos/_mixins/services/suwayomi/default.nix
+++ nixos/_mixins/services/suwayomi/default.nix
@@ -4,22 +4,25 @@
   pkgs,
   ...
 }:
-let 
-  installOn = ["workhorse"];
+let
+  installOn = [ "workhorse" ];
   basePath = "tachidesk";
-in lib.mkIf ( lib.elem "${hostname}" installOn): {
+in
+lib.mkIf (lib.elem "${hostname}" installOn) {
   services.suwayomi-server = {
-    enable=true;
+    enable = true;
   };
 
-  services.caddy = lib.mkIf (config.services.suwayomi-server.enable && config.services.tailscale.enable) {
-    enable = true;
-    virtualHosts."${hostname}.${tailNet}" = {
-      extraConfig = ''
-        handle_path ${basePath}/* {
-          reverse_proxy ${config.services.suwayomi-server.settings.ip}:${builtins.toString config.services.suwayomi-server.settings.port} {
-        }
-      '';
-    };
-  };
+  services.caddy =
+    lib.mkIf (config.services.suwayomi-server.enable && config.services.tailscale.enable)
+      {
+        enable = true;
+        virtualHosts."${hostname}.${tailNet}" = {
+          extraConfig = ''
+            handle_path ${basePath}/* {
+              reverse_proxy ${config.services.suwayomi-server.settings.ip}:${builtins.toString config.services.suwayomi-server.settings.port} {
+            }
+          '';
+        };
+      };
 }
