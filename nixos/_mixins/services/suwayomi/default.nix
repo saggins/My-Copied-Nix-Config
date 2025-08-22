3d246ff92b6a12ee3f22c750d3e01d7934e0a4d6
Sangmin Chun <sangmin@sagg.in>
2025-08-21 19:11:35 -0700

    dropping caddy support

--- nixos/_mixins/services/suwayomi/default.nix
+++ nixos/_mixins/services/suwayomi/default.nix
@@ -14,23 +14,8 @@ lib.mkIf (lib.elem "${hostname}" installOn) {
   services.suwayomi-server = {
     enable = true;
     settings.server = {
-      ip = "127.0.0.1";
+      ip = lib.mkIf (config.services.tailscale.enable) "${hostname}.${tailNet}" || "127.0.0.1";
       port = 25039;
     };
   };
-
-  services.caddy =
-    lib.mkIf (config.services.suwayomi-server.enable && config.services.tailscale.enable)
-      {
-        enable = true;
-        virtualHosts."${hostname}.${tailNet}" = {
-          extraConfig = ''
-            handle_path ${basePath}/* {
-              reverse_proxy ${config.services.suwayomi-server.settings.server.ip}:${builtins.toString config.services.suwayomi-server.settings.server.port} {
-                header_up Host ${config.services.actual.settings.hostname}:${builtins.toString config.services.actual.settings.port}
-              }
-            }
-          '';
-        };
-      };
 }
