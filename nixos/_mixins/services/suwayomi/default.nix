0cb45bccf07e3b12b3d99abfa80c2ec542710e99
Sangmin Chun <sangmin@sagg.in>
2025-08-21 19:51:08 -0700

    adding samba drive

--- nixos/_mixins/services/suwayomi/default.nix
+++ nixos/_mixins/services/suwayomi/default.nix
@@ -9,19 +9,47 @@
 let
   installOn = [ "workhorse" ];
   basePath = "/tachidesk";
+  smbPath = "/mnt/manga";
 in
 lib.mkIf (lib.elem "${hostname}" installOn) {
+  users.users.suwayomi-server = {
+    uid = 955;
+  };
   services.suwayomi-server = {
     enable = true;
     package = pkgs.unstable.suwayomi-server;
     settings.server = {
       ip = if config.services.tailscale.enable then "${hostname}.${tailNet}" else "127.0.0.1";
       port = 25039;
+      downloadAsCbz = true;
     };
   };
 
   services.komga = {
     enable = true;
     settings.server.port = 44857;
+  };
+
+  environment.systemPackages = lib.mkIf config.services.suwayomi-server.enable [ pkgs.cifs-utils ];
+  fileSystems."${smbPath}" = lib.mkIf config.services.suwayomi-server.enable {
+    device = "//10.0.0.166/manga";
+    fsType = "cifs";
+    options =
+      let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        user_perms = "uid=955,gid=1000,file_mode=0777,dir_mode=0777";
+      in
+      [ "${automount_opts},${user_perms},credentials=/etc/nixos/smb-secrets" ];
+  };
+  sops = {
+    secrets = {
+      workhorse-smb-secrets = lib.mkIf config.services.suwayomi-server.enable {
+        mode = "600";
+        owner = "root";
+        group = "root";
+        path = "/etc/nixos/smb-secrets";
+        sopsFile = ../../../../secrets/workhorse.yaml;
+      };
+    };
   };
 }
