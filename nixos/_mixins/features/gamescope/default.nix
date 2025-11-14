47115b37665b34d25cfdd4a013620de07c6dc9a9
Sangmin Chun <sangmin@sagg.in>
2025-11-13 16:10:31 -0800

    refactor

--- nixos/_mixins/features/gamescope/default.nix
+++ nixos/_mixins/features/gamescope/default.nix
@@ -1,6 +1,7 @@
 { pkgs, username, ... }:
-{ # taken from https://nixos.wiki/wiki/Steam#gamescope 
-# TODO: Enable username checking. just in case, multiple nixos becoems a thing
+{
+  # taken from https://nixos.wiki/wiki/Steam#gamescope
+  # TODO: Enable username checking. just in case, multiple nixos becoems a thing
   boot.kernelPackages = pkgs.linuxPackages;
   programs = {
     gamescope = {
@@ -17,7 +18,7 @@
 
   hardware.xone.enable = true; # xbox cvontroler
   environment = {
-    systemPackages = [pkgs.mangohud];
+    systemPackages = [ pkgs.mangohud ];
   };
-    
-}  
+
+}
