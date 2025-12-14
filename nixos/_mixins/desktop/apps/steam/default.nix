be84da4ad897b314313f77f3d7f79d84802963d2
Sangmin Chun <sangmin@sagg.in>
2025-12-13 17:35:37 -0800

    Fixing my dualsense controller

--- nixos/_mixins/desktop/apps/steam/default.nix
+++ nixos/_mixins/desktop/apps/steam/default.nix
@@ -21,7 +21,23 @@ lib.mkIf (lib.elem hostname installOn) {
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
   };
+
   services.udev.extraRules = ''
+
+    # Disable DualSense touchpad acting as mouse
+    # USB
+    ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
     ACTION=="add|change", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    # Bluetooth
+    ACTION=="add|change", ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+
+    # Disable DS4 touchpad acting as mouse
+    # USB
+    ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    ACTION=="add|change", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    # Bluetooth
+    ACTION=="add|change", ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
   '';
 }
