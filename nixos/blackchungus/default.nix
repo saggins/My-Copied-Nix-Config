9b91bae88a969350b52a3b34d2de3d8925b98998
Sangmin Chun <sangmin@sagg.in>
2026-06-17 14:11:23 -0700

    adding missing modules, why didn't the previous nixos generation catch keep this???, lookin to later

--- nixos/blackchungus/default.nix
+++ nixos/blackchungus/default.nix
@@ -24,6 +24,10 @@
       "uas"
       "sd_mod"
     ];
+    # bcachefs is out-of-tree on the 6.18 kernel; pull its module into the
+    # initrd so stage-1 can open the bcachefs root.
+    extraModulePackages = [ config.boot.kernelPackages.bcachefs ];
+    initrd.supportedFilesystems = [ "bcachefs" ];
     #    loader = {
     #      systemd-boot.enable = lib.mkForce false;
     #      grub = {
--- nixos/workhorse/default.nix
+++ nixos/workhorse/default.nix
@@ -23,6 +23,10 @@
       "uas"
       "sd_mod"
     ];
+    # bcachefs is out-of-tree on the 6.18 kernel; pull its module into the
+    # initrd so stage-1 can open the bcachefs root.
+    extraModulePackages = [ config.boot.kernelPackages.bcachefs ];
+    initrd.supportedFilesystems = [ "bcachefs" ];
   };
 
   nix.settings.trusted-users = ["sangmin"];
